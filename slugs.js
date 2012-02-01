714bf2cd06e39274f9a200ac4dafecef53ba89ec
tstrimple <tstrimple@gmail.com>
2012-02-01 14:34:32 -0800

    Added the ability to override the slug separator character

--- slugs.js
+++ slugs.js
@@ -3,11 +3,16 @@
  * By Aaron Stannard (aaron@stannardlabs.com)
  */
  
-var slug = module.exports = function slug (incString, preserved) {
+var slug = module.exports = function slug (incString, separator, preserved) {
     var p = ['.', '=', '-'];
+    var s = '-';
     
     if(typeof preserved != 'undefined') {
         p = preserved;
+    }
+
+    if(typeof separator != 'undefined') {
+        s = separator;
     }
 
     return incString.toLowerCase().
@@ -15,5 +20,5 @@ var slug = module.exports = function slug (incString, preserved) {
         replace(/^\s\s*/, '').replace(/\s\s*$/, '').    //  trim both sides of string
         replace(/[^\w\ ]/gi, '').   //  replaces all non-alphanumeric with empty string
         replace(/-{2,}/g, ' ').     //  remove duplicate spaces
-        replace(/[\ ]/gi, '-');    //  Convert spaces to dashes
+        replace(/[\ ]/gi, s);    //  Convert spaces to dashes
 }
