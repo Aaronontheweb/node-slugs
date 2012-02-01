c08160658a018978d0fc6703c3961e0c40d38a7e
Aaron Stannard <aaron@aaronstannard.com>
2012-02-01 08:59:26 +0000

    Fleshed out the trim unit tests

--- test/trim-tests.js
+++ test/trim-tests.js
@@ -5,6 +5,31 @@
  var trim = require('../lib/trim');
  
 exports['should-ltrim-space-from-string'] = function (test) {
-    test.equal(slugs(' thisisasafestring'), 'thisisasafestring');
+    test.equal(trim.ltrim(' thisisasafestring'), 'thisisasafestring');
+    test.done();
+};
+
+exports['should-rtrim-space-from-string'] = function (test) {
+    test.equal(trim.rtrim('thisisasafestring '), 'thisisasafestring');
+    test.done();
+};
+
+exports['should-trim-spaces-from-both-sides-of-string'] = function (test) {
+    test.equal(slugs(' thisisasafestring '), 'thisisasafestring');
+    test.done();
+};
+
+exports['should-not-modify-string-without-extra-whitespace'] = function (test) {
+    test.equal(trim.trim('thisisasafestring'), 'thisisasafestring');
+    test.done();
+};
+
+exports['should-trim-spaces-from-both-sides-of-string'] = function (test) {
+    test.equal(trim.trim('thisisasafestring'), 'thisisasafestring');
+    test.done();
+};
+
+exports['should-rtrim-spaces-dashes-from-string'] = function (test) {
+    test.equal(trim.trim('thisisasafestring-','-'), 'thisisasafestring');
     test.done();
 };
