761cd721eee2ccee49b08b529bce9719d65c2bcf
tstrimple <tstrimple@gmail.com>
2012-02-01 14:10:40 -0800

    Removed reference to underscore.js. Removed trim.js and replaced with inline trim using regex.

--- slugs.js
+++ slugs.js
@@ -3,12 +3,15 @@
  * By Aaron Stannard (aaron@stannardlabs.com)
  */
  
- var slug = module.exports = function slug (incString){
-     return incString.toLowerCase().
-        replace(/[.=-]/gi, ' ').    //  replace preserved characters with spaces
+var slug = module.exports = function slug (incString, preserved) {
+    if(preserved == 'undefined') {
+        preserved = ['.', '=', '-'];
+    }
+
+    return incString.toLowerCase().
+        replace('/[' + preserved.join('') + ']/gi', ' ').    //  replace preserved characters with spaces
         replace(/^\s\s*/, '').replace(/\s\s*$/, '').    //  trim both sides of string
         replace(/[^\w\ ]/gi, '').   //  replaces all non-alphanumeric with empty string
         replace(/-{2,}/g, ' ').     //  remove duplicate spaces
         replace(/[\ ]/gi, '-');    //  Convert spaces to dashes
- }
- 
+}
